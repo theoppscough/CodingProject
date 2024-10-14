af82322c37d62c793482a9a945d4744b39c59769
Kevin Ochoa <108596878+theoppscough@users.noreply.github.com>
2024-10-13 21:14:41 -0400

    Bug Fixes for npm start

--- server/app.js
+++ server/app.js
@@ -11,9 +11,9 @@ dotenv.config();
 const app = express();
 app.use(express.json()) // parse incoming JSON requests
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB connected'))
-    .catch(eer => console.log(err));
+    .catch(err => console.log(err));
 
 // Routes
 app.use('/api/users', userRoutes);
